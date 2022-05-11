7dbfe6b22e191cad5a801546ed9bca7781cc3b89
Loren Jamison <loren.jamison@gmail.com>
2022-05-10 19:01:20 -0500

    Removed unused exceptions.

--- src/main/java/io/github/sornerol/pdb4j/exception/PdbReaderException.java
+++
@@ -1,7 +0,0 @@
-package io.github.sornerol.pdb4j.exception;
-
-public class PdbReaderException extends Exception {
-    public PdbReaderException(String message) {
-        super(message);
-    }
-}
--- src/main/java/io/github/sornerol/pdb4j/exception/PdbWriterException.java
+++
@@ -1,7 +0,0 @@
-package io.github.sornerol.pdb4j.exception;
-
-public class PdbWriterException extends Exception {
-    public PdbWriterException(String message) {
-        super(message);
-    }
-}
